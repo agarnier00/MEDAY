386d56b445398aff2d94c1e25bd9c1e6dad2e2ce
agarnier00 <antoine.garnier@chuv.ch>
2016-11-30 09:19:34 +0100

    Add files via upload

---
+++ meday/Helpers/MyColors.cs
@@ -0,0 +1,14 @@
+﻿using MonoTouch.UIKit;
+
+namespace meday
+{
+	static public class MyColors
+	{
+		static public UIColor green = new UIColor (119.0f / 255.0f, 147.0f / 255.0f, 60.0f / 255.0f, 1.0f);
+		static public UIColor darkGreen = new UIColor (73.0f / 255.0f, 89.0f / 255.0f, 37.0f / 255.0f, 1.0f);
+		static public UIColor veryDarkGreen = new UIColor (42.0f / 255.0f, 51.0f / 255.0f, 21.0f / 255.0f, 1.0f);
+		static public UIColor red = new UIColor (157.0f / 255.0f, 43.0f / 255.0f, 43.0f / 255.0f, 1.0f);
+		static public UIColor darkRed = new UIColor (40.0f / 255.0f, 11.0f / 255.0f, 11.0f / 255.0f, 1.0f);
+	}
+}
+
